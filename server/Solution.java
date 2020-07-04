109ee26a73ce0ab3b1fd97765842d5e6eae2aebe
sarthaksadh01 <sarthaksadh01@gmail.com>
2020-07-05 02:21:52 +0530

    added question bank

--- server/Solution.java
+++ server/Solution.java
@@ -1,9 +1,6 @@
-
-    // "static void main" must be defined in a public class.
-   // Do not change the default java class name
-public class Solution {
-    public static void main(String[] args) {
-        System.out.println("Hello lol!");
-    }
-}
-    
+// "static void main" must be defined in a public class.
+ public class Solution {
+     public static void main(String[] args) {
+         System.out.println("Hello World!");
+     }
+ }
