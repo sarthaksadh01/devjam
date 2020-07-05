b749545353e9bb5d16911d4795606ebbec0847d6
sarthaksadh01 <sarthaksadh01@gmail.com>
2020-07-05 19:21:31 +0530

    server changed

--- week-1-client/src/data/data.js
+++ week-1-client/src/data/data.js
@@ -8,7 +8,8 @@ to database & functionalities.
 
 import axios from 'axios';
 // var baseUrl = "https://devjam-server.herokuapp.com/api";
-var baseUrl = "http://localhost:4000/api";
+var baseUrl = "http://139.59.91.217/api"
+// var baseUrl = "http://localhost:4000/api";
 async function getContent(id) {
     return new Promise((resolve, reject) => {
         axios.get(`${baseUrl}/content/${id}`).then((content) => {
